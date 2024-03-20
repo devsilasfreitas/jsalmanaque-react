8d1d12a972c67750857cc06195e87f26e2e8dc96
Silas <dev.silas.freitas@outlook.com>
2024-03-20 17:02:41 -0300

    fix: retorno do formatCode com a quebra de linha

--- frontend/src/functions/formatCode.js
+++ frontend/src/functions/formatCode.js
@@ -12,7 +12,7 @@ export function formatCode(text, convertOrDesconvert) {
       regex = /<pre><code class="language-(\w+)">([\s\S]*?)<\/code><\/pre>/g;
           replacement = (match, language, content) => {
             const modifiedContent = content.replace(/&lt;/g, '<').replace(/&gt;/g, '>');
-            return `<code ${language}>${modifiedContent}</code>`;
+            return `<code ${language}>\n${modifiedContent}</code>`;
         }
     }
   
